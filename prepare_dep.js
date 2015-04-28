06b7d9fcaf2984d613a15e0b0ab3707c0d0e85f2
vasily <elgesto@gmail.com>
2015-04-28 14:58:55 +0300

    Fix prepare

--- prepare_dep.js
+++ prepare_dep.js
@@ -6,23 +6,26 @@ var file = './full/full.json'
 var dataset = JSON.parse(fs.readFileSync(file,'UTF-8'))
 var files = []
 
-var types = ['body','title']
+var types = ['BODY','TITLE']
 
 _.each(dataset, function(value, key, list){ 
 
 	var id = value["$"]['NEWID']
 
-	_.each(types, function(type, key, list){ 
+	_.each(types, function(type, key1, list1){ 
+
 		if (type in value['TEXT'])
 		{
 			var data = value['TEXT'][type]
-			var data = data.replace(/\<\w*\.*\w*\>/g," ")
-			var data = data.replace(/\n/g," ")
-			var data = data.replace(/\s[uU]\.*[sS]\.*\s/g," USA ")
+			data = data.replace(/\<.*\>/g," ")
+			data = data.replace(/\n/g," ")
+			data = data.replace(/\s[uU]\.*[sS]\.*\s/g," USA ")
+			data = data.replace(/\s{2,}/g, ' ')
 
-			fs.writeFileSync('./full/full.json.dep/'+id+"."+"data", data)
 
-			files.push(__dirname+'/full/full.json.dep/'+id+"."+data)
+			fs.writeFileSync('./full/full.json.dep/'+id+"."+type, data)
+
+			files.push(__dirname+'/full/full.json.dep/'+id+"."+type)
 		}
 	}, this)
 	
