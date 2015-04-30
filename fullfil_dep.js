cdb64b3a60fbb4b2be60ece2cffeb4afbd01a8c9
vasily <elgesto@gmail.com>
2015-04-30 13:42:46 +0300

    Improve restore

--- fullfil_dep.js
+++ fullfil_dep.js
@@ -1,20 +1,70 @@
 var fs = require('fs');
 var _ = require('underscore')._;
 
-var dir = __dirname+'/full/full.json.parse/'
+var dir = './full/full.json.parse/'
 
 var files = fs.readdirSync(dir)
 
 var dataset = JSON.parse(fs.readFileSync('./R8/R8.train.json','UTF-8'))
 
-var dataset_hash = {}
+var conversion = {
+	'CharacterOffsetBegin':'characterOffsetBegin',
+	'CharacterOffsetEnd':'characterOffsetEnd',
+	'PartOfSpeech':'pos',
+	'Text':'word',
+	'Lemma':'lemma',
+	'NamedEntityTag':'ner',
+	'NormalizedNamedEntityTag':'nner'
+}
 
-// collapsed-ccprocessed-dependencies only
+function parseLine(line)
+{
+	var output = {}
+	line = line.replace(/[\[\]]/g,'')
+	line = line.split(" ")
+	_.each(line, function(param, key, list){ 
+		var pair = param.split("=")
+		if (!(pair[0] in conversion))
+			{
+			console.log(pair[0])
+			process.exit(0)
+			}
+		output[conversion[pair[0]]] = pair[1]
+	}, this)
+	return output
+}
 
-// to delete
-// basic-dependencies
-// collapsed-dependencies
-// tokens
+
+function replaceToken(json, id, type)
+{
+	var file = id + "." + type + ".out"
+	if (files.indexOf(file) == -1)
+	{
+		console.log("file " + file + " is not found in directory")
+		process.exit(0)
+	}
+
+	var out = fs.readFileSync(dir + file,'UTF-8')
+
+	lines = out.split("\n")
+
+	var sentences = []
+	var num = 0
+	_.each(lines, function(value, key, list){ 
+		if (!_.isNull(value.match(/^Sentence \#/)))
+			sentences.push([])
+
+		if (!_.isNull(value.match(/\[Text\=/)))
+			sentences[sentences.length - 1].push(parseLine(value))
+	}, this)
+
+	_.each(json['sentences'], function(sen, key, list){ 
+		json['sentences'][key]['tokens'] = sentences[key]
+	}, this)
+	return json
+}
+
+
 function parse_filter(parse)
 {
 	_.each(parse['sentences'], function(value, key, list){ 
@@ -25,64 +75,38 @@ function parse_filter(parse)
 	return parse
 }
 
+var types = ['BODY', 'TITLE']
+
+
 _.each(dataset, function(value, key, list){ 
-	dataset_hash[(value['$']['NEWID'])] = value
+
+	var id = value['$']['NEWID']
+
+	_.each(types, function(type, key, list){ 
+		var file = id+"."+type+".json"
+		
+
+		if (files.indexOf(file) != -1)
+		{
+			var json = parse_filter(JSON.parse(fs.readFileSync(dir + file,'UTF-8')))
+			json = replaceToken(json, id, type)
+
+			dataset[key][type + "_CORENLP" ] = json	
+		}
+
+	}, this)
 }, this)
 
-_.each(files, function(file, key, list){ 
+fs.writeFileSync('./R8/train/R8.train.corenlp.json', JSON.stringify(dataset, null, 4))
 
-	if (key % 1000 == 0)
-		console.log(key + " from " + files.length)
 
-	if (file == "README")
-		return
+// var data_splited = _.groupBy(dataset_new, function(element, index){
+// 	return index%15;
+// })
 
-	var names = file.split(".")
-
-	var id = names[0].toString()
-
-	if (id in dataset_hash)
-	{
-		dataset_hash[names[0]][names[1] + "_CORENLP" ] = parse_filter(JSON.parse(fs.readFileSync(dir + file,'UTF-8')))
-	}
-}, this)
-
-var dataset_new = []
-
-// body_dep
-// title_dep
-
-console.log("Organaize hash in array")
-
-var len = Object.keys(dataset_hash).length
-
-_.each(dataset_hash, function(value, key, list){ 
-
-	if (key % 1000 == 0)
-		console.log(key + " from " + len)
-
-	// if (!('body_dep' in value))
-	// {
-		// console.log(value['$']['NEWID']+" no body_dep")
-		// process.exit(0)
-	// }
-// 
-	// if (!('title_dep' in value))
-	// {
-		// console.log(value['$']['NEWID']+" no title_dep")
-		// process.exit(0)
-	// }
-
-	dataset_new.push(value)
-}, this)
-
-var data_splited = _.groupBy(dataset_new, function(element, index){
-	return index%15;
-})
-
-_.each(_.toArray(data_splited), function(data, key, list){
-	console.log("writing "+key)
-	fs.writeFileSync('./R8/train/R8.train.'+key+'.corenlp.json', JSON.stringify(data, null, 4))
-}, this)
+// _.each(_.toArray(data_splited), function(data, key, list){
+// 	console.log("writing "+key)
+// 	fs.writeFileSync('./R8/train/R8.train.'+key+'.corenlp.json', JSON.stringify(data, null, 4))
+// }, this)
 
 
